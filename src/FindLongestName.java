3642c4dafaef14e6237e3d962ae35a46239bc97f
pockocmoc <pockocmoc@inbox.ru>
2022-07-21 12:47:31 +0300

    Initial commit

---
+++ src/FindLongestName.java
@@ -0,0 +1,25 @@
+public class FindLongestName {
+    public static void main(String[] args) {
+
+
+    }
+
+    public String findLongestName(String[] names) {
+        int size = names.length;
+        String longestName = new String();
+        longestName = names[0];
+
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].length() > names.length) ;
+
+
+
+        }return longestName;
+
+
+    }
+}
+
+
+
+
